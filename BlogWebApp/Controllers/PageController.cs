4a0fd54d4b56d2de8a0da3d42c02646753c07842
bisal-xetri <bishaldkl56@gmail.com>
2025-01-27 12:06:35 +0545

    Added UI and Controllers

---
+++ BlogWebApp/Controllers/PageController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebApp.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+
+    }
+}
