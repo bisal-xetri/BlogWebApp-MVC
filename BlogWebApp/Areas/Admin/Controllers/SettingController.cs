aea0c1302391cf5f145f7f0ba414348aacdeaba9
bisal-xetri <bishaldkl56@gmail.com>
2025-01-30 17:18:06 +0545

    setting Module complete

--- BlogWebApp/Areas/Admin/Controllers/SettingController.cs
+++ BlogWebApp/Areas/Admin/Controllers/SettingController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using BlogWebApp.Data;
+using BlogWebApp.Models;
+using BlogWebApp.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogWebApp.Areas.Admin.Controllers
 {
@@ -7,9 +12,98 @@ namespace BlogWebApp.Areas.Admin.Controllers
     [Authorize(Roles ="Admin")]
     public class SettingController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly INotyfService _notification;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public SettingController(ApplicationDbContext context, INotyfService notyfService, IWebHostEnvironment webHostEnvironment )
         {
-            return View();
+            _context = context;
+            _notification = notyfService;
+            _webHostEnvironment = webHostEnvironment;
         }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var settings = _context.Settings.ToList();
+            if( settings.Count > 0 )
+            {
+                var vm = new SettingVM()
+                {
+                    Id= settings[0].Id,
+                    Title=settings[0].Title,
+                    ThumbnailUrl = settings[0].ThumbnailUrl,
+                    SiteName=settings[0].SiteName,
+                    ShortDescription=settings[0].ShortDescription,
+                    TwitterUrl=settings[0].TwitterUrl,
+                    FacebookUrl=settings[0].FacebookUrl,
+                    GitHubUrl=settings[0].GitHubUrl,
+                };
+                return View( vm );
+            }
+            var setting = new Setting()
+            {
+               
+                SiteName = "Demo Name",
+            };
+            await _context.Settings.AddAsync(setting);
+            await _context.SaveChangesAsync();
+            var createdSetting = _context.Settings.ToList();
+            var createdvm = new SettingVM()
+            {
+                Id = createdSetting[0].Id,
+                Title = createdSetting[0].Title,
+                ThumbnailUrl = createdSetting[0].ThumbnailUrl,
+                SiteName = createdSetting[0].SiteName,
+                ShortDescription = createdSetting[0].ShortDescription,
+                TwitterUrl = createdSetting[0].TwitterUrl,
+                FacebookUrl = createdSetting[0].FacebookUrl,
+                GitHubUrl = createdSetting[0].GitHubUrl,
+            };
+            return View(createdvm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(SettingVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+            var setting= await _context.Settings.FirstOrDefaultAsync(x=>x.Id==vm.Id);
+            if(setting == null)
+            {
+                _notification.Error("Something went Wrong");
+                return View(vm);
+            }
+            setting.SiteName=vm.SiteName;
+            setting.Title=vm.Title;
+            setting.ShortDescription=vm.ShortDescription;
+            setting.TwitterUrl=vm.TwitterUrl;
+            setting.FacebookUrl=vm.FacebookUrl;
+            setting.GitHubUrl=vm.GitHubUrl;
+            if(vm.Thumbnail!=null)
+            {
+                setting.ThumbnailUrl=UploadImage(vm.Thumbnail);
+
+            }
+           
+            await _context.SaveChangesAsync();
+            _notification.Success("Setting Updated Successfully");
+            return RedirectToAction("Index");
+        }
+        private string UploadImage(IFormFile file)
+        {
+            string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "Thumbnail");
+            var filePath = Path.Combine(folderPath, uniqueFileName);
+
+            using (var fileStream = System.IO.File.Create(filePath))
+            {
+                file.CopyTo(fileStream);
+            }
+
+            return uniqueFileName;
+        }
+
     }
 }
--- BlogWebApp/ViewModels/SettingVM.cs
+++ BlogWebApp/ViewModels/SettingVM.cs
@@ -1,19 +1,18 @@
-﻿
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BlogWebApp.Models
+namespace BlogWebApp.ViewModels
 {
-    public class Setting
+    public class SettingVM
     {
         public int Id { get; set; }
         public string? SiteName { get; set; }
         public string? Title { get; set; }
         public string? ShortDescription { get; set; }
         public string? ThumbnailUrl { get; set; }
-        public string? FacebookUrl{ get; set; }
+        public string? FacebookUrl { get; set; }
         public string? GitHubUrl { get; set; }
-        public string? TwitterUrl{ get;set; }
-     
-      
+        public string? TwitterUrl { get; set; }
+       
+        public IFormFile? Thumbnail { get; set; }
     }
 }
