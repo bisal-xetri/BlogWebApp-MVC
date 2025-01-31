c5a19b120829e8ab8160d014bfcc916fabb671f3
bisal-xetri <bishaldkl56@gmail.com>
2025-01-31 11:31:16 +0545

    home page design completed

--- BlogWebApp/Controllers/HomeController.cs
+++ BlogWebApp/Controllers/HomeController.cs
@@ -1,21 +1,35 @@
 using System.Diagnostics;
+using BlogWebApp.Data;
 using BlogWebApp.Models;
+using BlogWebApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogWebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var vm = new HomeVM();
+            var setting= _context.Settings.ToList();
+            vm.Title = setting[0].Title;
+            vm.ShortDescription = setting[0].ShortDescription;
+            vm.ThumbnailUrl = setting[0].ThumbnailUrl;
+            vm.Posts=_context.Posts!.Include(x=>x.ApplicationUser).ToList();
+             
+                return View(vm);
+       
+            
         }
 
         public IActionResult Privacy()
---
+++ BlogWebApp/ViewModels/HomeVM.cs
@@ -0,0 +1,14 @@
+﻿using BlogWebApp.Models;
+
+namespace BlogWebApp.ViewModels
+{
+    public class HomeVM
+    {
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? ShortName { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public List<Post>? Posts { get; set; }
+
+    }
+}
