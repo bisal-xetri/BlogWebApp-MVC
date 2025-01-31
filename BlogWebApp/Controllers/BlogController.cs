6735b05dd60614571a9e18f9ceff38ed5bfae463
bisal-xetri <bishaldkl56@gmail.com>
2025-01-31 12:33:11 +0545

    post Module Added

---
+++ BlogWebApp/Controllers/BlogController.cs
@@ -0,0 +1,50 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using BlogWebApp.Data;
+using BlogWebApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogWebApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly INotyfService _notification;
+        public BlogController(ApplicationDbContext context, INotyfService notyfyservice)
+        {
+            _context = context; 
+            _notification = notyfyservice;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Post(string slug)
+        {
+            if (string.IsNullOrEmpty(slug))
+            {
+                _notification.Error("Post not Found");
+                return View();
+            }
+
+            var post = await _context.Posts.Include(x => x.ApplicationUser)
+                                           .FirstOrDefaultAsync(x => x.Slug == slug);
+            if (post == null)
+            {
+                _notification.Error("Post not Found");
+                return View();
+            }
+
+            var vm = new BlogPostVM()
+            {
+                Id = post.Id,
+                Title = post.Title,
+                AuthorName = post.ApplicationUser!.FirstName + " " + post.ApplicationUser.LastName,
+                CreatedDate = (DateTime)post.CreatedDate,
+                Description = post.Description,
+                ThumbnailUrl=post.ThumbnailUrl,
+                ShortDescription= post.ShortDescription,
+            };
+
+            return View(vm);
+        }
+
+    }
+}
